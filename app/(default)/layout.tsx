7166ef78c434e586434918838b21a36c6fbd21ba
Jeremiah Nueno <jeremiahnueno2017@gmail.com>
2026-02-09 16:26:06 +0800

    ts ignore

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "@/components/theme-provider";
+// @ts-ignore
 import "../globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
