fe22e5d9aad887bf585529273341dd0422ed1dd2
Jeremiah Nueno <jeremiahnueno2017@gmail.com>
2026-02-17 09:48:34 +0800

    [AUTH] Fix failed to fetch for cors

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,7 +6,7 @@ const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db(process.env.MONGODB_DATABASE!);
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_BASE_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_BASE_URL,
   secret: process.env.BETTER_AUTH_SECRET,
   database: mongodbAdapter(db, {
     client, 
